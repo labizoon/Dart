fd8366020f7e5324513d4017b8df6554d43954a8
labizoon <labimirza465@gmail.com>
2025-08-11 06:40:30 +0000

    Conditional statments

---
+++ d4.dart
@@ -0,0 +1,32 @@
+//conditional statments
+void main(){
+    
+    //if/else
+
+    int score =45;
+    if(score<50){
+        print('your score is less than 50');
+    }
+    else{
+        print('your score is more than 50');
+    }
+
+
+
+   //switch statement example
+  String grade = 'B';
+
+switch (grade) {
+  case 'A':
+    print('Outstanding!');
+    break;
+  case 'B':
+    print('Well done!');
+    break;
+  case 'C':
+    print('Can improve.');
+    break;
+  default:
+    print('Invalid grade.');
+}
+}
