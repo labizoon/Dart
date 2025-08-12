54d8a52be7523bb2145c50e526d383fc9cfe8094
labizoon <labimirza465@gmail.com>
2025-08-12 16:02:14 +0000

    Interpolation ❤️

---
+++ d7.dart
@@ -0,0 +1,20 @@
+//String Interpolation
+//Definition: Interpolation lets you insert variables or expressions directly into strings using $variable or ${expression}.
+
+
+void main() {
+  String name = 'Zara';
+  int age = 24;
+  int score = 95;
+
+  // 🔹 Basic interpolation
+  print('Hello, $name');
+
+  // 🔹 Expression interpolation
+  print('Next year you’ll be ${age + 1} years old');
+
+  // 🔹 Mixing text and numbers
+  print('Your score is $score out of 100');
+
+  
+}
